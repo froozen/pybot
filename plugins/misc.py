e6d33bbf4c7673e9789e1a84dc4996a5dae2767a
fro_ozen <fro_ozen@gmx.de>
2014-03-14 19:25:33 +0100

    roll and eroll now roll a 6-faced dice by default

--- plugins/misc.py
+++ plugins/misc.py
@@ -7,11 +7,13 @@ def cmd_roll(parsed_input):
     if len(split) > 1:
         try:
             end = int(split[1])
-            connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(0, end))
+            connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(1, end))
 
         except ValueError:
             connection.send_privmsg(parsed_input["channel"], "Invalind input: %s." % split[1])
 
+    else:
+        connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(1, 6))
 
 def cmd_eroll(parsed_input):
     split = parsed_input["content"].split(" ")
@@ -19,8 +21,11 @@ def cmd_eroll(parsed_input):
     if len(split) > 1:
         try:
             end = long(split[1])
-            connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(0, end))
+            connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(1, end))
 
         except ValueError:
             connection.send_privmsg(parsed_input["channel"], "Invalind input: %s." % split[1])
+
+    else:
+        connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(1, 6))
 
