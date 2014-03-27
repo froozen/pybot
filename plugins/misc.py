ceb40c9084b6b32080678ef096fe133507618e2b
fro_ozen <fro_ozen@gmx.de>
2014-03-27 18:28:16 +0100

    Removed eroll as it proved to be unnecessary

--- plugins/misc.py
+++ plugins/misc.py
@@ -15,17 +15,3 @@ def cmd_roll(parsed_input):
     else:
         connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(1, 6))
 
-def cmd_eroll(parsed_input):
-    split = parsed_input["content"].split(" ")
-
-    if len(split) > 1:
-        try:
-            end = long(split[1])
-            connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(1, end))
-
-        except ValueError:
-            connection.send_privmsg(parsed_input["channel"], "Invalind input: %s." % split[1])
-
-    else:
-        connection.send_privmsg(parsed_input["channel"], "You rolled %d." % random.randint(1, 6))
-
