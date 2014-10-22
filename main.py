a113b1c031405cfacc30ade78e47fae9712ca254
fro_ozen <fro_ozen@gmx.de>
2014-10-22 16:05:12 +0200

    Remove all the old code

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python2
 
-import connection
-import plugin_handler
-import logging
-
-logging.init()
-connection.init()
-plugin_handler.init()
-
-while True:
-    readlines = connection.read_lines()
-    
-    for readline in readlines:
-        plugin_handler.handle_input(readline)
-
-connection.disconnect()
+print "Fresh new start!"
