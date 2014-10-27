df2f352093e6e684c5a76ab272d354dc63eb8ff4
fro_ozen <fro_ozen@gmx.de>
2014-10-27 17:41:23 +0100

    Add list access methods to shared_data

--- pybot/shared_data.py
+++ pybot/shared_data.py
@@ -37,3 +37,40 @@ def get_data_container ( key ):
 
     # Simply call the method in _container
     return _container.get_data_container ( key )
+
+def append ( self, key, value ):
+    """Append a value to a list responding to a key.
+
+    Keys:
+        \"user.email\" means root [ "user" ] [ "email" ]
+
+    This method is thread safe.
+    """
+
+    # Simply call the method in _container
+    _container.append ( key, value )
+
+def pop ( self, key, index ):
+    """Pop the value at an index off a list responding to a key.
+
+    Keys:
+        \"user.email\" means root [ "user" ] [ "email" ]
+
+    This method is thread safe.
+    """
+
+    # Simply call the method in _container
+    _container.pop ( key, index )
+
+def remove ( self, key, value ):
+    """Remove a value from a list responding to a key.
+
+    Keys:
+        \"user.email\" means root [ "user" ] [ "email" ]
+
+    This method is thread safe.
+    """
+
+    # Simply call the method in _container
+    _container.remove ( key, value )
+
